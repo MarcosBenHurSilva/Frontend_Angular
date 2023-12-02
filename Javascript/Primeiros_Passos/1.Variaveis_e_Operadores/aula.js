32ce946a865ce9bacc2df146ef3d73607a72944a
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-12-02 18:39:14 -0300

    exercicio primeira aula

---
+++ Javascript/Primeiros_Passos/1.Variaveis_e_Operadores/aula.js
@@ -0,0 +1,16 @@
+
+const pi = 3.14;
+// pi = 5.15; // Isso causará o erro TypeError: Assignment to constant variable.
+
+let pi2 = 3.14;
+pi2 = 5.15; // Isso é válido com let, mas não com const.
+
+let variavel = 10 + 10;
+
+console.log(pi);
+console.log(pi2);
+console.log(variavel);
+
+
+
+//console.log('Imprimir algo');
