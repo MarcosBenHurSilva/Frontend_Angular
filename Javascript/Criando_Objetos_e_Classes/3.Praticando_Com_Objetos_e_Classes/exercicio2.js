51bd2e23d36adb50db650e68d1bf0875f2ab1553
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-12-03 19:43:37 -0300

    Exercicio 2

--- Javascript/Criando_Objetos_e_Classes/3.Praticando_Com_Objetos_e_Classes/exercicio2.js
+++ Javascript/Criando_Objetos_e_Classes/3.Praticando_Com_Objetos_e_Classes/exercicio2.js
@@ -5,3 +5,20 @@ As pessoas devem ter a capacidade de dizer o valor do seu IMC (IMC = peso / (alt
 Instancie uma pessoa chamada José que tenha 70kg de peso e 1,75 de altura e peça ao José para dizer o valor
 do seu IMC;
 */
+class Pesoa{
+    nome;
+    peso;
+    altura;
+    constructor(nome, peso, altura) {
+        this.nome = nome;
+        this.peso = peso;
+        this.altura = altura;
+    }
+    calcularImc() {
+        return this.peso / (Math.pow(this.altura, 2));
+    }
+}
+
+const jose = new Pesoa('José', 70.0, 1.75)
+
+console.log(`Meu nome é ${jose.nome} e meu IMC é: ${jose.calcularImc().toFixed(2).replace('.',',')}.`)
