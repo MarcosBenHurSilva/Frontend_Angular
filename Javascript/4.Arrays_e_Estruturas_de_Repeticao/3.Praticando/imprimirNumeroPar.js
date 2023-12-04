8e42442a1fa1b4a780851744a8bb4cdc81e0735b
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-12-04 02:20:40 -0300

    Imprimir Numeros Pares

--- Javascript/4.Arrays_e_Estruturas_de_Repeticao/3.Praticando/imprimirNumeroPar.js
+++ Javascript/4.Arrays_e_Estruturas_de_Repeticao/3.Praticando/imprimirNumeroPar.js
@@ -3,7 +3,7 @@
 lista de números e imprima cada número Par encontrado.
 */
 
-const nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+const nums = [12,21,32,45,56,64,79,82,93,107];
 
 for (let i = 0; i < nums.length; i++) {
     const num = nums[i];
