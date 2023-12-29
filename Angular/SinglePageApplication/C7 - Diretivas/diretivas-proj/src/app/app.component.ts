66ce10777da1ee5fc21163330254e7fffd1b97b0
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-12-28 23:20:25 -0300

    update

--- Angular/SinglePageApplication/C7 - Diretivas/diretivas-proj/src/app/app.component.ts
+++ Angular/SinglePageApplication/C7 - Diretivas/diretivas-proj/src/app/app.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CardComponent } from './card/card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'diretivas-proj';
+
 }
