60098639fb56902bd35147ab505212338dbba6a3
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2024-05-16 19:55:18 -0300

    atualizações

--- Javascript/7.Projeto_Pokedex/assets/js/main.js
+++ Javascript/7.Projeto_Pokedex/assets/js/main.js
@@ -1,7 +1,7 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
 
-const maxRecords = 251
+const maxRecords = 386
 const limit = 100
 let offset = 0;
 
